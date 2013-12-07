393720496c1deba602a26a3d90126c9f130be1bf
Jonathan Beilin <jbeilin@gmail.com>
2013-12-06 22:36:45 -0800

    BVAppDelegate: begin migration

--- BVAppDelegate.m
+++ BVAppDelegate.m
@@ -7,43 +7,110 @@
 //
 
 #import "BVAppDelegate.h"
+#import "OverlayViewController.h"
+#import "cocos2d.h"
+#import "PongVaderScene.h"
 
 @implementation BVAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
+    _window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    // Init the window
+	self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+	
+	// cocos2d will inherit these values
+	[_window setUserInteractionEnabled:YES];
+	[_window setMultipleTouchEnabled:YES];
+	
+	// Try to use CADisplayLink director
+	// if it fails (SDK < 3.1) use the default director
+	if( ! [CCDirector setDirectorType:CCDirectorTypeDisplayLink] )
+		[CCDirector setDirectorType:CCDirectorTypeDefault];
+	
+	// Use RGBA_8888 buffers
+	// Default is: RGB_565 buffers
+	[[CCDirector sharedDirector] setPixelFormat:kPixelFormatRGBA8888];
+	
+	// Create a depth buffer of 16 bits
+	// Enable it if you are going to use 3D transitions or 3d objects
+    //	[[CCDirector sharedDirector] setDepthBufferFormat:kDepthBuffer16];
+	
+	// Default texture format for PNG/BMP/TIFF/JPEG/GIF images
+	// It can be RGBA8888, RGBA4444, RGB5_A1, RGB565
+	// You can change anytime.
+	[CCTexture2D setDefaultAlphaPixelFormat:kTexture2DPixelFormat_RGBA8888];
+	
+	// before creating any layer, set the landscape mode
+	[[CCDirector sharedDirector] setDeviceOrientation:CCDeviceOrientationPortrait];
+	[[CCDirector sharedDirector] setAnimationInterval:1.0/60];
+	[[CCDirector sharedDirector] setDisplayFPS:NO];
+	
+	// create an openGL view inside a window
+	[[CCDirector sharedDirector] attachInView:_window];
+	[_window makeKeyAndVisible];
+	
+	[[CCDirector sharedDirector] runWithScene: (CCScene *) [PongVader scene]];
+	
+	[[OverlayViewController sharedController] show];
+    
+	[[OverlayViewController sharedController] hide];
+    
+	// jump into the initial state
+    [GameState handleEvent: [[[StateMainMenu alloc] init] autorelease]];
+    
+    _window.backgroundColor = [UIColor blackColor];
+    [_window makeKeyAndVisible];
     return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+	[[PongVader getInstance] terminating];
+	[[CCDirector sharedDirector] stopAnimation];
+	[[CCDirector sharedDirector] pause];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+	[[PongVader getInstance] terminating];
+	[[CCDirector sharedDirector] stopAnimation];
+	[[CCDirector sharedDirector] pause];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+	[[CCDirector sharedDirector] stopAnimation]; // call this to make sure you don't start a second display link!
+	[[CCDirector sharedDirector] resume];
+	[[CCDirector sharedDirector] startAnimation];
+	//[GameState handleEvent: [[[StateMainMenu alloc] init] autorelease]];
+    
+	PongVader *pv = [PongVader getInstance];
+	
+	GameState *cur = [GameState getCurrentState];
+	if ([cur isKindOfClass:[StateGetReady class]] ||
+		[cur isKindOfClass:[StatePlaying class]] ||
+		[cur isKindOfClass:[StatePostPlaying class]] ||
+		[cur isKindOfClass:[StateMainMenu class]] ||
+		[cur isKindOfClass:[StateMovie class]]) {
+		
+        [GameState handleEvent: [[[StateMainMenu alloc] init] autorelease]];
+	}
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+- (void)applicationWillTerminate:(UIApplication *)application {
+	[[PongVader getInstance] terminating];
+	[[CCDirector sharedDirector] end];
+}
+
+- (void)applicationSignificantTimeChange:(UIApplication *)application {
+	[[CCDirector sharedDirector] setNextDeltaTimeZero:YES];
 }
 
 @end
