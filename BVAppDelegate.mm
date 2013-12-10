4ac38756c0979b51e4d8ef00bbed6f84c9324c9f
Jonathan Beilin <jbeilin@gmail.com>
2013-12-09 19:29:16 -0800

    BVAppDelegate: make obj-c++

--- BVAppDelegate.mm
+++ BVAppDelegate.mm
@@ -13,50 +13,51 @@
 
 @implementation BVAppDelegate
 
+@synthesize window = _window;
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     _window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    
-    // Init the window
-	self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
 	
 	// cocos2d will inherit these values
 	[_window setUserInteractionEnabled:YES];
 	[_window setMultipleTouchEnabled:YES];
-	
-	// Try to use CADisplayLink director
-	// if it fails (SDK < 3.1) use the default director
-//	if( ! [CCDirector setDirectorType:CCDirectorTypeDisplayLink] )
-//		[CCDirector setDirectorType:CCDirectorTypeDefault];
-	
-	// Use RGBA_8888 buffers
-	// Default is: RGB_565 buffers
-	[[CCDirector sharedDirector] setPixelFormat:kPixelFormatRGBA8888];
-	
-	// Create a depth buffer of 16 bits
-	// Enable it if you are going to use 3D transitions or 3d objects
-    //	[[CCDirector sharedDirector] setDepthBufferFormat:kDepthBuffer16];
-	
-	// Default texture format for PNG/BMP/TIFF/JPEG/GIF images
-	// It can be RGBA8888, RGBA4444, RGB5_A1, RGB565
-	// You can change anytime.
-	[CCTexture2D setDefaultAlphaPixelFormat:kTexture2DPixelFormat_RGBA8888];
-	
-	// before creating any layer, set the landscape mode
-	[[CCDirector sharedDirector] setDeviceOrientation:CCDeviceOrientationPortrait];
-	[[CCDirector sharedDirector] setAnimationInterval:1.0/60];
-	[[CCDirector sharedDirector] setDisplayFPS:NO];
-	
-	// create an openGL view inside a window
-	[[CCDirector sharedDirector] attachInView:_window];
-	[_window makeKeyAndVisible];
-	
-	[[CCDirector sharedDirector] runWithScene: (CCScene *) [PongVader scene]];
-	
-	[[OverlayViewController sharedController] show];
     
-	[[OverlayViewController sharedController] hide];
+    CCGLView *glView = [CCGLView viewWithFrame:[_window bounds]
+								   pixelFormat:kEAGLColorFormatRGBA8
+								   depthFormat:0
+							preserveBackbuffer:NO
+									sharegroup:nil
+								 multiSampling:NO
+							   numberOfSamples:0];
+    [glView setMultipleTouchEnabled:YES];
     
+    CCDirectorIOS *dir = (CCDirectorIOS *) [CCDirector sharedDirector];
+	
+	dir.wantsFullScreenLayout = YES;
+	[dir setDisplayStats:NO];
+	[dir setAnimationInterval:1.0/60];
+	[dir setView:glView];
+	[dir setProjection:kCCDirectorProjection2D];
+	if( ! [dir enableRetinaDisplay:YES] )
+		CCLOG(@"Retina Display Not supported");
+	[CCTexture2D setDefaultAlphaPixelFormat:kCCTexture2DPixelFormat_RGBA8888];
+	
+	// If the 1st suffix is not found and if fallback is enabled then fallback suffixes are going to searched. If none is found, it will try with the name without suffix.
+	// On iPad HD  : "-ipadhd", "-ipad",  "-hd"
+	// On iPad     : "-ipad", "-hd"
+	// On iPhone HD: "-hd"
+	CCFileUtils *sharedFileUtils = [CCFileUtils sharedFileUtils];
+	[sharedFileUtils setEnableFallbackSuffixes:NO];				// Default: NO. No fallback suffixes are going to be used
+	[sharedFileUtils setiPhoneRetinaDisplaySuffix:@"-hd"];		// Default on iPhone RetinaDisplay is "-hd"
+	[sharedFileUtils setiPadSuffix:@"-ipad"];					// Default on iPad is "ipad"
+	[sharedFileUtils setiPadRetinaDisplaySuffix:@"-ipadhd"];	// Default on iPad RetinaDisplay is "-ipadhd"
+	
+	// Assume that PVR images have premultiplied alpha
+	[CCTexture2D PVRImagesHavePremultipliedAlpha:YES];
+	
+    [dir runWithScene: (CCScene *) [PongVader scene]];
+	
 	// jump into the initial state
     [GameState handleEvent: [[StateMainMenu alloc] init]];
     
@@ -89,9 +90,6 @@
 	[[CCDirector sharedDirector] stopAnimation]; // call this to make sure you don't start a second display link!
 	[[CCDirector sharedDirector] resume];
 	[[CCDirector sharedDirector] startAnimation];
-	//[GameState handleEvent: [[[StateMainMenu alloc] init] autorelease]];
-    
-	PongVader *pv = [PongVader getInstance];
 	
 	GameState *cur = [GameState getCurrentState];
 	if ([cur isKindOfClass:[StateGetReady class]] ||
@@ -107,6 +105,7 @@
 - (void)applicationWillTerminate:(UIApplication *)application {
 	[[PongVader getInstance] terminating];
 	[[CCDirector sharedDirector] end];
+    CC_DIRECTOR_END();
 }
 
 - (void)applicationSignificantTimeChange:(UIApplication *)application {
