68b2fa77d1d01cf7850e9aeee44e4445934157dc
Jonathan Beilin <jbeilin@gmail.com>
2013-12-09 21:01:57 -0800

    Invader: remove parts of dummy init

--- Classes/Invader.mm
+++ Classes/Invader.mm
@@ -73,7 +73,7 @@
 		[animFrames addObject:[[CCSpriteFrameCache sharedSpriteFrameCache] spriteFrameByName:[NSString stringWithFormat:@"%@_walk%d.png", invType, i]]];
 	}
 //	invader.idle = [CCAnimation animationWithName:@"idle" delay:GAME_SPB/3.0f frames:animFrames];
-    invader.idle = [CCAnimation animationWithAnimationFrames:animFrames delayPerUnit:GAME_SPB/3.0f loops:0];
+//    invader.idle = [CCAnimation animationWithAnimationFrames:animFrames delayPerUnit:GAME_SPB/3.0f loops:0];
 	
 	if (w) [invader createBodyInWorld: w];
 	
